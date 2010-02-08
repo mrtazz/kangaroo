9a72aa0953caa2599dcafd4ea6b04c3f57e8d9a7
Daniel <d@lordofhosts.de>
2010-02-08 21:54:51 +0100

    add Client App to utilize Calendar Library

--- technology_scouting/calender_api/calendar_interface/src/com/example/kangaroo/calendar_interface/calendar_interface.java
+++ technology_scouting/calender_api/calendar_interface/src/com/example/kangaroo/calendar_interface/calendar_interface.java
@@ -1,7 +1,14 @@
 package com.example.kangaroo.calendar_interface;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class calendar_interface extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +16,31 @@ public class calendar_interface extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        ArrayList<String> al; // = new ArrayList<String>();
+        ArrayAdapter<String> aa;
+        TextView tv = (TextView)findViewById(R.id.foo_text);
+        ListView lv = (ListView)findViewById(R.id.foo_list);
+
+        tv.setText("who dat?");
+        CalendarLibrary cl = new CalendarLibrary(this);
+
+        HashMap<String, CalendarEvent> events;
+        
+        CalendarEvent ev = new CalendarEvent("3", "InsertTest", "here", null, null,
+        									new Date(), new Date(), false,
+        									false, false, "I am a description", 1, "GMT");
+        
+        cl.addEventToBackend(ev);
+        
+        tv.setText("who dat?");
+        // get events
+        events = cl.getEventsFromBackend(new String("1"));
+        al = new ArrayList<String>(events.keySet()); 
+        
+	    // Bind the ListView to an ArrayList of strings.
+	  	aa = new ArrayAdapter<String>(getApplicationContext(), 
+	  	                              android.R.layout.simple_list_item_1,
+ 	                                  al);
+	  	lv.setAdapter(aa);
     }
 }
