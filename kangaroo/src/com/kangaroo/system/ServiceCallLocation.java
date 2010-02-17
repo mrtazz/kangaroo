9774eaa04ec7fd1bd4f3f7e17965fe440f2b6a4a
alex <a@rebooten.de>
2010-02-17 17:52:23 +0100

    added location service listener

--- kangaroo/src/com/kangaroo/system/ServiceCallLocation.java
+++ kangaroo/src/com/kangaroo/system/ServiceCallLocation.java
@@ -2,12 +2,53 @@ package com.kangaroo.system;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 
 public class ServiceCallLocation extends Service
 {
 
+	private LocationManager locationManager;
+	
+	@Override
+	public void onCreate() 
+	{
+		 locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		 locationManager.requestLocationUpdates("gps", 0, 0, locationListener);
+	}
+	
 	//TODO register at LocatonProvider, so taht we are called when the location has changed X
+	
+	private LocationListener locationListener = new LocationListener() {
+
+		@Override
+		public void onLocationChanged(Location location) 
+		{
+			
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) 
+		{
+			
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) 
+		{
+			
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) 
+		{
+			
+		}
+		
+	};
 	
 	@Override
 	public IBinder onBind(Intent intent) 
@@ -16,6 +57,7 @@ public class ServiceCallLocation extends Service
 		return null;
 	}
 
+	
 	
 	//TODO implement Thread that is executed on location events
 	
--- kangaroo/src/com/kangaroo/system/ServiceCallTick.java
+++ kangaroo/src/com/kangaroo/system/ServiceCallTick.java
@@ -2,10 +2,8 @@ package com.kangaroo.system;
 
 import com.kangaroo.gui.AlertUserInteraction;
 import com.kangaroo.system.ServiceCallTick;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
@@ -14,49 +12,48 @@ import android.os.Parcel;
 import android.os.RemoteException;
 import android.os.SystemClock;
 
-public class ServiceCallTick extends Service
+/**
+ * This class provides a means to execute tasks that need to be called periodically. 
+ * This service is started by "BroadcastReceiverBootTime" after the OS has finished loading.
+ * 
+ * @author alex
+ */
+public class ServiceCallTick extends android.app.Service
 {
 	private PendingIntent mAlarmSender = null;
 	private int callIntervall = 10;
 	private AlertUserInteraction myAlertService; 
-	private int temp=0;
 	
+	/**
+	 * This method is part of the Service-interface. It is called when an instance of the service
+	 * is created. In this instance the registration of the service with the AlarmManager for periodical
+	 * re-calling is done here.
+	 */
     @Override
     public void onCreate() 
     {
-        //if(mAlarmSender == null) //only the first time!
-        //{
-        	temp = 1;
         	myAlertService = new AlertUserInteraction(this);
         	mAlarmSender = PendingIntent.getService(ServiceCallTick.this, 0, new Intent(ServiceCallTick.this, ServiceCallTick.class), 0);
         	 // call us again every callIntervall seconds
             long firstTime = SystemClock.elapsedRealtime();
             AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
             am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstTime, callIntervall*1000, mAlarmSender);
-        //}
-        
+            
         Thread thr = new Thread(null, mTask, "ServiceCallTick Worker Thread");
         thr.start();
     }
 
-    @Override
-    public void onDestroy() 
-    {
-
-    }
-
     /**
-     * The function that runs in our worker thread
+     * When the Service is called by the AlarmManager, this function is executed in a new thread. 
+     * (so that we don't disturb the GUI or other things running in the main thread)
      */
     Runnable mTask = new Runnable() 
     {
         public void run() 
         {
             //TODO add work here
-        	System.out.println("text"+String.valueOf(temp));
         	Looper.prepare();
-        	myAlertService.showToast("text"+String.valueOf(temp));
-        	temp++;
+        	myAlertService.showToast("text");
         	
         	//TODO remove sleep-cycle
             long endTime = System.currentTimeMillis() + 5*1000;
@@ -77,15 +74,51 @@ public class ServiceCallTick extends Service
             ServiceCallTick.this.stopSelf();
         }
     };
+    
+    
+    /**
+     * return the callIntervall variable. callIntervall specifies, that the service should be called every x seconds.
+     * @return int: callIntervall
+     */
+    public int getCallIntervall() 
+    {
+		return callIntervall;
+	}
 
+    /**
+     * set the callIntervall variable. callIntervall specifies, that the service should be called every x seconds.
+     * @param int: callIntervall
+     */
+	public void setCallIntervall(int callIntervall) 
+	{
+		this.callIntervall = callIntervall;
+	}
+
+	/**
+	 * remove the service from the AlarmManager. If this is called, the service will no longer be started periodically.
+	 */
     public void stopScheduled()
     {
-    	//TODO implement detaching from Alarm-manager
+    	//TODO test detaching from Alarm-manager
+    	if(mAlarmSender != null)
+    	{
+        	AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
+       	 	am.cancel(mAlarmSender);
+    	}   
+    }
+	
+    /**
+     * Call this when the service is terminated. (part of the Service interface)
+     */
+	@Override
+    public void onDestroy() 
+    {
+
     }
     
-    
-    
-    
+	/**
+	 * Return this object to interact with the service.
+	 */
     @Override
     public IBinder onBind(Intent intent) 
     {
