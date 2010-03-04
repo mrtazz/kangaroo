7c7d2777fa3bbb0eb1bd90b13e7944d89582cdf8
Daniel <d@unwiredcouch.com>
2010-03-04 11:53:37 +0100

    add basic context menu for dayplan events

--- kangaroo/src/com/kangaroo/gui/DayPlan.java
+++ kangaroo/src/com/kangaroo/gui/DayPlan.java
@@ -7,15 +7,20 @@ import java.util.ArrayList;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.AdapterView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.kangaroo.R;
 import com.kangaroo.calendar.CalendarEvent;
 import com.kangaroo.calendar.CalendarLibrary;
+import com.l2fprod.common.propertysheet.PropertySheetTableModel.Item;
 
 /**
  * @author mrtazz
@@ -27,17 +32,68 @@ public class DayPlan extends ListActivity {
 	  private CalendarAdapter calendarAdapter;
 	  private TextView tv;
 	  private CalendarLibrary cl;
-
+	  // menu item ids
+	  private final int MENU_DELETE = 0;
+	  private final int MENU_TO_TASK = 1;
+	  private final int MENU_ADD_LOCATION = 2;
+	  
 	  @Override
 	  public void onCreate(Bundle savedInstanceState)
 	  {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.dayplan);
+	        registerForContextMenu(getListView());
 	        cl = new CalendarLibrary(this);
 	        tv = (TextView)findViewById(R.id.DayTitle);
 
 	        reload();
 	  }
+	  public void onCreateContextMenu(ContextMenu menu,
+			  						  View v,
+			  						  ContextMenuInfo menuInfo)
+	  {
+		  AdapterView.AdapterContextMenuInfo info;
+		  try {
+		      info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+		  } catch (ClassCastException e) {
+		      //Log.e(TAG, "bad menuInfo", e);
+		      return;
+		  }
+		  //long id = getListAdapter().getItemId(info.position);
+		  // add menu items
+		  menu.add(0, MENU_DELETE, 0, R.string.delete);
+		  menu.add(0, MENU_TO_TASK, 0, R.string.to_task);
+		  menu.add(0, MENU_ADD_LOCATION, 0, R.string.add_location);
+	  }
+	  /** when press-hold option selected */
+	  @Override
+	  public boolean onContextItemSelected(MenuItem item) {
+	    return applyMenuChoice(item) || super.onContextItemSelected(item);
+	  }
+	  
+	  private boolean applyMenuChoice(MenuItem item) {
+		  Toast toast;
+		  switch (item.getItemId()) {
+		    case MENU_DELETE:
+			  toast = Toast.makeText(this,
+			  					     "Menu item# "+Long.toString(item.getItemId())+" clicked",
+				  				     Toast.LENGTH_SHORT);
+		  	  toast.show();
+		      return true;
+		    case MENU_ADD_LOCATION:
+			  toast = Toast.makeText(this,
+  					   				 "Menu item# "+Long.toString(item.getItemId())+" clicked",
+  					   				 Toast.LENGTH_SHORT);
+			  toast.show();
+		      return true;
+		    case MENU_TO_TASK:
+			  toast = Toast.makeText(this,
+  					   				 "Menu item# "+Long.toString(item.getItemId())+" clicked",
+  					   				 Toast.LENGTH_SHORT);
+			  toast.show();
+		  }
+		  return false;
+		}
 
 	  private void reload()
 	  {
