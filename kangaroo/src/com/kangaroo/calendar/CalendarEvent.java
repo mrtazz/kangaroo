a37cebe36617d7bdb77e7bb5389320c753db5c85
alex <a@rebooten.de>
2010-03-16 17:06:07 +0100

    new fix for serializing Tasks in Events

--- kangaroo/src/com/kangaroo/calendar/CalendarEvent.java
+++ kangaroo/src/com/kangaroo/calendar/CalendarEvent.java
@@ -113,7 +113,7 @@ public class CalendarEvent {
 		this.endDate = endDate;
 		this.wasTask = true;
 		this.allDay = null;
-		this.description = task.toString();
+		this.description = task.getDescription()+"\n---\n"+task.serialize();
 		this.calendar = -1;
 		this.timezone = null;
 		this.place = here;
--- kangaroo/src/com/kangaroo/task/Task.java
+++ kangaroo/src/com/kangaroo/task/Task.java
@@ -250,8 +250,8 @@ public class Task
 	@Override
 	public String toString() {
 		if (name != null) {
-			//return "Task: {name = " + name + "}";
-			return name + "\n---\n"+this.serialize();
+			return "Task: {name = " + name + "}";
+			//return name + "\n---\n"+this.serialize();
 		}
 		return super.toString();
 	}
